3d378fe13016bf80eca928072e925d4ee761bebe
kakack <kakack@126.com>
2015-03-23 00:21:53 -0700

    2015-3-22

---
+++ CodeList/Pascal's Triangle II .cpp
@@ -0,0 +1,30 @@
+/*
+Pascal's Triangle II 
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+*/
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        
+        vector<int> result;
+        long long cur = 1;
+        result.push_back(cur);
+        
+
+            for(int i=1;i<=rowIndex;i++){
+              cur=cur*(rowIndex-i+1)/i;
+              
+              result.push_back(cur);
+              }  
+
+        return result;
+        
+    }
+};
