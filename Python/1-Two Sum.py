55cea0b3f51d866fb6f510546b9d2a4a9ecc022b
kakack <kakack@126.com>
2020-03-09 22:47:35 +0800

    20200309

--- Python/1-Two Sum.py
+++ Python/1-Two Sum.py
@@ -9,6 +9,21 @@
 # return [0, 1].
 
 
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         length=len(nums)
+#         for i in range(0, length):
+#             for j in range(i+1, length):
+#                 if nums[i]+nums[j]==target:
+#                     return i, j
+#                 else:
+#                     continue
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -16,15 +31,15 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        length=len(nums)
-        for i in range(0, length):
-            for j in range(i+1, length):
-                if nums[i]+nums[j]==target:
-                    return i, j
-                else:
-                    continue
+        hashmap = {}
+        for i, num in enumerate(nums):
+            hashmap[num] = i
 
-min
+        for i, num in enumerate(nums):
+            j = hashmap.get(target - num)
+            if j and i != j:
+                return [i, j]
+
 # if __name__ == '__main__':
 #     nums = [2, 7, 11, 15]
 #     target = 9
