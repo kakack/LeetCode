fc7580d74b114b9b3a491be7158265c4ad5e4cf4
Chen Kai <kakack@126.com>
2020-04-18 10:35:28 +0800

    20200418

--- Python/14-Longest Common Prefix.py
+++ Python/14-Longest Common Prefix.py
@@ -12,3 +12,23 @@ class Solution:
             if len(set(letter_group)) > 1:
                 return strs[0][:i]
             return min(strs)
+        
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ''
+        ans = strs[0]
+        for str in strs[1:]:
+            i = 0
+            while i < len(ans) and i < len(str):
+                if ans[i] != str[i]:
+                    break
+                i += 1
+            ans = ans[:i]
+            if ans == '':
+                return ans
+        return ans
