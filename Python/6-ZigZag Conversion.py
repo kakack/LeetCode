4cac20ff552d94a4d7b9fb5e2cac913407626469
Chen Kai <kakack@126.com>
2020-05-14 11:43:34 +0800

    20200514

--- Python/6-ZigZag Conversion.py
+++ Python/6-ZigZag Conversion.py
@@ -1,14 +1,27 @@
 # The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
-
+#
 # P   A   H   N
 # A P L S I I G
 # Y   I   R
 # And then read line by line: "PAHNAPLSIIGYIR"
+#
 # Write the code that will take a string and make this conversion given a number of rows:
-
-# string convert(string text, int nRows);
-# convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
-# 字符串处理
+#
+# string convert(string s, int numRows);
+# Example 1:
+#
+# Input: s = "PAYPALISHIRING", numRows = 3
+# Output: "PAHNAPLSIIGYIR"
+# Example 2:
+#
+# Input: s = "PAYPALISHIRING", numRows = 4
+# Output: "PINALSIGYAHRPI"
+# Explanation:
+#
+# P     I    N
+# A   L S  I G
+# Y A   H R
+# P     I
 
 class Solution(object):
     def convert(self, s, numRows):
@@ -17,20 +30,25 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
-        if numRows == 1 or numRows >= len(s):
+        if numRows == 1:
             return s
-        res = [''] * numRows
-        # 生成numRows个空字符串
-        index, steps = 0, 1
+        row = [""] * min(numRows, len(s))
+        curRow = 0
+        goingDown = False
 
-        for x in s:
-            res[index] += x
-            if index == 0:
-                steps = 1
-            # 如果在第一排，那么从上往下
-            elif index == numRows -1:
-                steps = -1
-            index += steps
-            # 如果在最后一排，那么从下往上
-        return ''.join(res)
-        # 把字符串list整合成一个字符串
+        for c in s:
+            row[curRow] += c
+            if curRow == 0 or curRow == numRows - 1:
+                goingDown = not goingDown
+            if goingDown:
+                curRow += 1
+            else:
+                curRow -= 1
+        res = ''
+        for st in row:
+            res += st
+        return res
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.convert("PAYPALISHIRING", 3))
