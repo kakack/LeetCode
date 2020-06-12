ec129659854ce7a8e1836a8bbce96758ba37f3f0
Chen Kai <kakack@126.com>
2020-06-12 10:50:37 +0800

    20200612

--- Python/15-3Sum.py
+++ Python/15-3Sum.py
@@ -1,9 +1,13 @@
-# Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
-
-# Note: The solution set must not contain duplicate triplets.
-
-# For example, given array S = [-1, 0, 1, 2, -1, -4],
-
+# Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+#
+# Note:
+#
+# The solution set must not contain duplicate triplets.
+#
+# Example:
+#
+# Given array nums = [-1, 0, 1, 2, -1, -4],
+#
 # A solution set is:
 # [
 #   [-1, 0, 1],
@@ -17,14 +21,34 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
+        n = len(nums)
+        if n < 3:
+            return []
         nums.sort()
-        res = []
-        for i in range(len(nums)-2):
-            for j in range(i+1, len(nums)-1):
-                target = 0 - nums[i] - nums[j]
-                if target in nums[j+1:]:
-                    res.append((nums[i], nums[j], target))
-        return list(set(res))
+        result = []
+        for i in range(n):
+            if nums[i] > 0:
+                return result
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            L = i + 1
+            R = n - 1
+            while L < R:
+                if nums[i] + nums[L] + nums[R] == 0:
+                    result.append([nums[i], nums[L], nums[R]])
+                    while L < R and nums[L] == nums[L + 1]:
+                        L += 1
+                    while L < R and nums[R] == nums[R - 1]:
+                        R -= 1
+                    L = L + 1
+                    R = R - 1
+                elif nums[i] + nums[L] + nums[R] > 0:
+                    R = R - 1
+                else:
+                    L = L + 1
+        return result
 
-# 这个办法容易超时
-
+if __name__ == '__main__':
+    s = Solution()
+    nums = [-1,0,1,2,-1,-4]
+    print(s.threeSum(nums))
