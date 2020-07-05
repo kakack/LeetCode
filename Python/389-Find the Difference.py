1e52e2c6531aa3f01fefa88504ef24e8aa1b95d3
kakack <kakack@126.com>
2020-07-05 18:39:35 +0800

    20200705

--- Python/389-Find the Difference.py
+++ Python/389-Find the Difference.py
@@ -34,42 +34,42 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        hashMap = {"a": 0,
-                   "b": 0,
-                   "c": 0,
-                   "d": 0,
-                   "e": 0,
-                   "f": 0,
-                   "g": 0,
-                   "h": 0,
-                   "i": 0,
-                   "j": 0,
-                   "k": 0,
-                   "l": 0,
-                   "m": 0,
-                   "n": 0,
-                   "o": 0,
-                   "p": 0,
-                   "q": 0,
-                   "r": 0,
-                   "s": 0,
-                   "t": 0,
-                   "u": 0,
-                   "v": 0,
-                   "w": 0,
-                   "x": 0,
-                   "y": 0,
-                   "z": 0}
-        for c in s:
-            hashMap[c] += 1
-        for c in t:
-            if hashMap.get(c) != 0:
-                hashMap[c] -= 1
-            else:
-                return c
-        for k in hashMap.keys():
-            if hashMap[k] != 0:
-                return k
+        # hashMap = {"a": 0,
+        #            "b": 0,
+        #            "c": 0,
+        #            "d": 0,
+        #            "e": 0,
+        #            "f": 0,
+        #            "g": 0,
+        #            "h": 0,
+        #            "i": 0,
+        #            "j": 0,
+        #            "k": 0,
+        #            "l": 0,
+        #            "m": 0,
+        #            "n": 0,
+        #            "o": 0,
+        #            "p": 0,
+        #            "q": 0,
+        #            "r": 0,
+        #            "s": 0,
+        #            "t": 0,
+        #            "u": 0,
+        #            "v": 0,
+        #            "w": 0,
+        #            "x": 0,
+        #            "y": 0,
+        #            "z": 0}
+        # for c in s:
+        #     hashMap[c] += 1
+        # for c in t:
+        #     if hashMap.get(c) != 0:
+        #         hashMap[c] -= 1
+        #     else:
+        #         return c
+        # for k in hashMap.keys():
+        #     if hashMap[k] != 0:
+        #         return k
 
 
 
