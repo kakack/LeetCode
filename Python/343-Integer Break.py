e2e435b8faf82298f6acd82d74e0e6abd2de78a7
Chen Kai <kakack@126.com>
2020-07-30 14:49:12 +0800

    20200730

--- Python/343-Integer Break.py
+++ Python/343-Integer Break.py
@@ -26,4 +26,9 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        return n - 1 if n < 4 else 3 ** (n // 3 - 1) * {3: 3, 4: 4, 5: 6}[(n - (n // 3 - 1) * 3)]
+        # return n - 1 if n < 4 else 3 ** (n // 3 - 1) * {3: 3, 4: 4, 5: 6}[(n - (n // 3 - 1) * 3)]
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            for j in range(i):
+                dp[i] = max(dp[i], j * (i - j), j * dp[i - j])
+        return dp[n]
