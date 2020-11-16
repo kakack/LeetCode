2eb3fd03d51c2116abd0a8d8b657c02f618a8501
Chen Kai <kakack@126.com>
2020-11-16 11:09:53 +0800

    20201116

---
+++ Python/406-Queue Reconstruction by Height.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*
+"""
+@author: Kai Chen
+@file: 406-Queue Reconstruction by Height.py
+@time: 2020/11/16 10:13
+@desc:
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+
+Example
+
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+
+"""
+
+
+class Solution(object):
+    def reconstructQueue(self, people):
+        """
+        :type people: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        people.sort(key=lambda x: (x[0], -x[1]))
+        # print(people)
+        n = len(people)
+        ans = [[] for _ in range(n)]
+        for person in people:
+            spaces = person[1] + 1
+            for i in range(n):
+                if not ans[i]:
+                    spaces -= 1
+                    if spaces == 0:
+                        ans[i] = person
+                        break
+        return ans
+
+
+if __name__ == '__main__':
+    s = Solution()
+    ipt = [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]
+    print(s.reconstructQueue(ipt))
