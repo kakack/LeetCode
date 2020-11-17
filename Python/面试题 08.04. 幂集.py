7f5fc8e852322eac0c6a1384fe68b0a069504d05
Chen Kai <kakack@126.com>
2020-11-17 11:07:35 +0800

    20201117

--- Python/面试题 08.04. 幂集.py
+++ Python/面试题 08.04. 幂集.py
@@ -43,6 +43,28 @@ class Solution1(object):
 
 
 # 回溯
+"""
+private void backtrack("原始参数") {
+    //终止条件(递归必须要有终止条件)
+    if ("终止条件") {
+        //一些逻辑操作（可有可无，视情况而定）
+        return;
+    }
+
+    for (int i = "for循环开始的参数"; i < "for循环结束的参数"; i++) {
+        //一些逻辑操作（可有可无，视情况而定）
+
+        //做出选择
+
+        //递归
+        backtrack("新的参数");
+        //一些逻辑操作（可有可无，视情况而定）
+
+        //撤销选择
+    }
+}
+
+"""
 class Solution2(object):
     def subsets(self, nums):
         """
