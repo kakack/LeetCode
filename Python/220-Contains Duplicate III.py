247dce839c8fad965dc10c88247f96ea2d7e9dfc
Chen Kai <kakachen1990@gmail.com>
2021-04-17 19:50:09 +0800

    20210417

--- Python/220-Contains Duplicate III.py
+++ Python/220-Contains Duplicate III.py
@@ -1,19 +1,37 @@
-# Given an array of integers,
-# find out whether there are two distinct indices i and j in the array
-# such that the absolute difference between nums[i] and nums[j] is at most t
-# and the absolute difference between i and j is at most k.
+# -*- coding: utf-8 -*
+"""
+@author: Kai Chen
+@file: 220-Contains Duplicate III.py
+@time: 2021/4/17 19:14
+@desc:
+Given an integer array nums and two integers k and t, return true if there are two distinct indices i and j in the array such that abs(nums[i] - nums[j]) <= t and abs(i - j) <= k.
 
-# Example 1:
-# Input: nums = [1,2,3,1], k = 3, t = 0
-# Output: true
 
-# Example 2:
-# Input: nums = [1,0,1,1], k = 1, t = 2
-# Output: true
 
-# Example 3:
-# Input: nums = [1,5,9,1,5,9], k = 2, t = 3
-# Output: false
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3, t = 0
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1, t = 2
+Output: true
+Example 3:
+
+Input: nums = [1,5,9,1,5,9], k = 2, t = 3
+Output: false
+
+
+Constraints:
+
+0 <= nums.length <= 2 * 104
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 104
+0 <= t <= 231 - 1
+"""
+
+import sys
+from sortedcontainers import SortedSet
 
 class Solution(object):
     def containsNearbyAlmostDuplicate(self, nums, k, t):
@@ -23,20 +41,18 @@ class Solution(object):
         :type t: int
         :rtype: bool
         """
-        hashMap = {}
-        for i, item in enumerate(nums):
-            for value in hashMap:
-            # for value in range(item - t, item + t + 1):
-            #     index = hashMap.get(value)
-                index = hashMap.get(value)
-                if abs(value - item) <= t and abs(index - i) <= k:
-                    return True
-            hashMap[item] = i
+        st = SortedSet()
+        left, right = 0, 0
+        res = 0
+        while right < len(nums):
+            if right - left > k:
+                st.remove(nums[left])
+                left += 1
+            index = bisect.bisect_left(st, nums[right] - t)
+            if st and index >= 0 and index < len(st) and abs(st[index] - nums[right]) <= t:
+                return True
+            st.add(nums[right])
+            right += 1
         return False
 
-if __name__ == '__main__':
-    s = Solution()
-    print(s.containsNearbyAlmostDuplicate([2,1], 1, 1))
-    print(s.containsNearbyAlmostDuplicate([1, 2, 3, 1], 3, 0))
-    print(s.containsNearbyAlmostDuplicate([1, 0, 1, 1], 1, 2))
-    print(s.containsNearbyAlmostDuplicate([1, 5, 9, 1, 5, 9], 2, 3))
+
