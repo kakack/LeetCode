76d2b6b8827ee04805849c86a1f7d9336e023716
Chen Kai <kakachen1990@gmail.com>
2021-05-17 11:56:00 +0800

    20210517

--- C++/12-Integer to Roman.cpp
+++ C++/12-Integer to Roman.cpp
@@ -60,7 +60,7 @@ public:
         vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         vector<string> numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         string list = "";
-        for (int i = 0; i <= values.size(); i ++) {
+        for (int i = 0; i < values.size(); i ++) {
             while (num >= values[i]) {
                 num -= values[i];
                 list.append(numerals[i]);
