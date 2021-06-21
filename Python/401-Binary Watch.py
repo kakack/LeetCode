e9de4f15208343c4d52267ec59276e6e4f1c558b
Chen Kai <kakachen1990@gmail.com>
2021-06-21 15:20:50 +0800

    20210621

--- Python/401-Binary Watch.py
+++ Python/401-Binary Watch.py
@@ -1,38 +1,49 @@
 # -*- coding: utf-8 -*
 """
-@author: Kaka Chen
-@license: (C) Copyright Personal Uncommercial Limited.
-@contact: kakachen1990@gmail.com
+@author: Kai Chen
 @file: 401-Binary Watch.py
-@time: 2020/6/25 6:26 下午
+@time: 2021/6/21 14:53
 @desc:
-A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent the minutes (0-59).
+A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent the minutes (0-59). Each LED represents a zero or one, with the least significant bit on the right.
 
-Each LED represents a zero or one, with the least significant bit on the right.
+For example, the below binary watch reads "4:51".
 
 
-For example, the above binary watch reads "3:25".
+Given an integer turnedOn which represents the number of LEDs that are currently on, return all possible times the watch could represent. You may return the answer in any order.
 
-Given a non-negative integer n which represents the number of LEDs that are currently on, return all possible times the watch could represent.
+The hour must not contain a leading zero.
 
-Example:
+For example, "01:00" is not valid. It should be "1:00".
+The minute must be consist of two digits and may contain a leading zero.
 
-Input: n = 1
-Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
-Note:
-The order of output does not matter.
-The hour must not contain a leading zero, for example "01:00" is not valid, it should be "1:00".
-The minute must be consist of two digits and may contain a leading zero, for example "10:2" is not valid, it should be "10:02".
+For example, "10:2" is not valid. It should be "10:02".
 
+
+Example 1:
+
+Input: turnedOn = 1
+Output: ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
+Example 2:
+
+Input: turnedOn = 9
+Output: []
+
+
+Constraints:
+
+0 <= turnedOn <= 10
 """
 
+
 class Solution(object):
-    hour = [1, 2, 4, 8]
-    minute = [1, 2, 4, 8, 16, 32]
-    def readBinaryWatch(self, num):
+    def readBinaryWatch(self, turnedOn):
         """
-        :type num: int
+        :type turnedOn: int
         :rtype: List[str]
         """
-        return [f"{h}:{m:02d}" for h in range(12) for m in range(60) if (bin(h) + bin(m)).count('1') == num]
-
+        ans = []
+        for h in range(12):
+            for m in range(60):
+                if (bin(h) + bin(m)).count('1') == turnedOn:
+                    ans.append(f"{h}:{m:02d}")
+        return ans
